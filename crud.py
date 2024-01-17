e3debe8bc4e2718ad98b3b31ca970d29236dc817
unknown <juniorjogos9169@gmai.com>
2024-01-17 10:40:24 -0300

    funcao-if

--- crud.py
+++ crud.py
@@ -1,52 +1,63 @@
 import mysql.connector
 
-conexao = mysql.connector.connect( #estou usando um servidor local então para funcionar é preciso que preencha com os dados do seu proprio servidor.
+conexao = mysql.connector.connect(
     host='localhost',
     user='root',
-    password='102030',
+    password='!nteligenci@',
     database='crud',
 )
 cursor = conexao.cursor()
+#--------estou usando um servidor local então para funcionar é preciso que preencha com os dados do seu proprio servidor.----------------------------------------------------------------------------------
+#----------------------------------funções--------------------------------------------------------
 
-#crud
+def create():
+    nome_produto = input('Digite o nome do produto que você deseja adicionar:  ')
+    valor = int(input('Digite quantas unidades deste: '))
+    comando = f'INSERT INTO vendas (nome_produto, valor) VALUES ("{nome_produto}", {valor})'
+    cursor.execute(comando)
+    conexao.commit()  # edita no banco de dados
+    resultado = cursor.fetchall()  # ler o banco de dados
 
-nome_produto = "patrimonio"
-valor = 1
-comando = f'INSERT INTO vendas (nome_produto, valor) VALUES ("{nome_produto}", {valor})'
-cursor.execute(comando)
-conexao.commit() # edita no banco de dados
-resultado = cursor.fetchall() # ler o banco de dados
+
+#------------------------------------------------------------------------------------------
+def read():
+    comando = f'SELECT * FROM vendas'
+    cursor.execute(comando)
+    resultado = cursor.fetchall()
+    print(resultado)
 
 
+#------------------------------------------------------------------------------------------
+def update():
+    nome_produto = input('Digite o nome do produto que deseja alterar: ')
+    valor = input('Quantidade: ')
+    comando = f'UPDATE vendas SET valor = {valor} WHERE nome_produto = "{nome_produto}"'
+    cursor.execute(comando)
+    conexao.commit()  # edita o banco de dados
+
+
+#------------------------------------------------------------------------------------------
+def delete():
+    nome_produto = input('Digite o iten que deseja excluir: ')
+    comando = f'DELETE FROM vendas WHERE nome_produto = "{nome_produto}"'
+    cursor.execute(comando)
+    conexao.commit()
+
+
+#---------------------------------crud Programa principal---------------------------------------------------------
+
+escolha = input('Digite ( 1 ) para ver a lista\n Digite ( 2 ) para adicionar itens a lista\n Digite ( 3 ) para editar itens da lista\nDigite ( 4 ) para excluir itens da lista\n')
+if escolha =='1':
+    read()
+elif escolha == '2':
+    create()
+elif escolha == '3':
+    update()
+elif escolha == '4':
+    delete()
+else:
+    print("Nenhuma das opções que voce digitou é válido\n certifique que digitou corretamente o número que corresponda o comando.")
 
 cursor.close()
 conexao.close()
-
-
- # CREATE
-# nome_produto = "computador"
-#valor = 3
-#comando = f'INSERT INTO vendas (nome_produto, valor) VALUES ("{nome_produto}", {valor})'
-#cursor.execute(comando)
-#conexao.commit() # edita no banco de dador
-#resultado = cursor.fetchall() # ler o banco de dados
-
- # READ
-#omando = f'SELECT * FROM vendas'
-#cursor.execute(comando)
-#resultado = cursor.fetchall()
-#print(resultado)
-
-
- # UPDATE
-#nome_produto = "computador"
-#valor = 2
-#comando = f'UPDATE vendas SET valor = {valor} WHERE nome_produto = "{nome_produto}"'
-#cursor.execute(comando)
-#conexao.commit() #edita o banco de dados
-
- # DELETE
-#nome_produto = "computador"
-#comando =f'DELETE FROM vendas WHERE nome_produto = "{nome_produto}"'
-#cursor.execute(comando)
-#conexao.commit()
+#------------------------------------------------------------------------------------------
